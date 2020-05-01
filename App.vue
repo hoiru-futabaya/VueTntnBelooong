45cda12aedfed1a0189eb433db0b426e2fc726c4
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-05-01 20:18:32 +0900

    とりあえず伸びるだけ

---
+++ App.vue
@@ -0,0 +1,54 @@
+<template>
+    <view class="container">
+    <text>お</text>
+   <text>ち</text>
+   <text>ん</text>
+   <text>ち</text>
+   <text>ん</text>
+   <text>び</text>
+   <text>ろ</text>
+    <text v-for="(message, index) in messages" :key="index">{{message}}</text>
+    <text>ん</text>
+        <touchable-opacity class="button" :on-press="addTodo">
+            <text class="button-text">追加</text>
+        </touchable-opacity>
+    </view>
+</template>
+<script>
+    export default {
+       data: function() {
+         return {
+           messageText: '｜',
+           messages: ['｜']
+      }
+    },
+        methods: {
+          addTodo: function() {
+          this.messages.push(this.messageText)
+            }
+          },
+        }
+</script>
+
+<style>
+.container {
+  background-color: white;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+.button {
+  background-color: pink;
+  padding: 10px;
+  border-radius: 5;
+  justify-content: center;
+  align-items: center;
+  }
+
+.main {
+  flex: 1;
+  max-width: 400px;
+  align-items: center;
+}
+</style>
